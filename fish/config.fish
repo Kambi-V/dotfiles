035e148fc063f44ab55639df132c50a63bda5a3f
Kambi-V <erratickambi@gmail.com>
2025-01-18 22:09:14 +0300

    refactor(fish): update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,25 +1,25 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-set -gx EDITOR 'nvim'
+
+set -gx EDITOR nvim
+set -U fish_greeting ""
+
+alias g='git'
 alias vi='nvim'
 alias vim='nvim'
 alias python=python3
+alias icat='kitty +kitten icat'
+alias v='npx vitest run'
+alias fd='fdfind'
+alias rg='rgrep'
 
-function nvm
-  bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
-end
+kubectl completion fish | source
 
-# Flutter directory installed with snapd
-set -gx PATH $PATH /snap/bin/flutter
+set -gx GPG_TTY (tty)
 
 # android command-lite-tools
 set -x ANDROID_HOME ~/Android
-set -x PATH $ANDROID_HOME/cmdline-tools/9.0/bin $PATH
-# >>> JVM installed by coursier >>>
-set -gx JAVA_HOME "/home/kambi/.cache/coursier/arc/https/github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_linux_hotspot_8u292b10.tar.gz/jdk8u292-b10"
-set -gx PATH "$PATH:/home/kambi/.cache/coursier/arc/https/github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_linux_hotspot_8u292b10.tar.gz/jdk8u292-b10/bin"
-# <<< JVM installed by coursier <<<
 
 # sdkman configs
 function sdk
@@ -29,10 +29,18 @@ end
 # >>> coursier install directory >>>
 set -gx PATH "$PATH:/home/kambi/.local/share/coursier/bin"
 # <<< coursier install directory <<<
-set PATH $PATH (find ~/.sdkman/candidates/maven/current/bin -maxdepth 0)
+#
+# set PATH $PATH (find ~/.sdkman/candidates/maven/current/bin -maxdepth 0)
+fish_add_path (find "$HOME/.sdkman/candidates/maven/current/bin" -maxdepth 0)
+fish_add_path (find "$HOME/.sdkman/candidates/kotlin/current/bin" -maxdepth 0)
+fish_add_path (find "$HOME/.sdkman/candidates/java/current/bin" -maxdepth 0)
+fish_add_path (find "$HOME/.sdkman/candidates/gradle/current/bin" -maxdepth 0)
+fish_add_path (find "$HOME/Android/platform-tools/adb" -maxdepth 0)
 
-set PATH $PATH (find ~/.sdkman/candidates/kotlin/current/bin -maxdepth 0)
-set -x JAVA_HOME /usr/lib/jvm/java-17-openjdk-amd64
+# set PATH $PATH (find ~/.sdkman/candidates/kotlin/current/bin -maxdepth 0)
+# set PATH $PATH (find ~/.sdkman/candidates/java/current/bin -maxdepth 0)
+
+set -x JAVA_HOME $HOME/.sdkman/candidates/java/current
 set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
 
 # pyenv(python version manager) path
@@ -47,46 +55,46 @@ set -xg PATH "$MODULAR_HOME/pkg/packages.modular.com_mojo/bin" $PATH
 #conda linked to fish
 source /home/kambi/miniconda3/etc/fish/conf.d/conda.fish
 
- # traverses project tree till .nvmrc is located
-function find_up
-    set path (pwd)
-    while test $path != "/"
-        and not test -e "$path/$argv[1]"
-        set path (dirname $path)
-    end
-    if test -e "$path/$argv[1]"
-        echo $path
-    else
-        echo ""
-    end
-end
-function __check_nvm --on-variable PWD
-    if status --is-command-substitution
-        return
-    end
-    set nvm_path (find_up ".nvmrc" | tr -d '[:space:]')
-    if test "$nvm_path" != ""
-        set nvmrc_node_version (nvm version (cat "$nvm_path/.nvmrc") '; 2>1')
-        if test "$nvmrc_node_version" = "N/A"
-            echo "Installing node version "(cat "$nvm_path/.nvmrc")
-            nvm install
-            set nvm_node_version (nvm version)
-        else if test "$nvmrc_node_version" != (nvm version)
-            echo "Changing node version to $nvmrc_node_version"
-            nvm use
-            set nvm_node_version (nvm version)
-        end
-        echo "$nvm_path/.nvmrc"
-    else if test (nvm version) = "none"
-        nvm use default --silent
-    # else if test (nvm version) != (nvm version default)
-        # echo "Reverting node version to default"
-        # nvm use default
-    end
-end
-__check_nvm
-
-bass source ~/.nvm/nvm.sh > /dev/null ^ /dev/null;
+# traverses project tree till .nvmrc is located
+# function find_up
+#     set path (pwd)
+#     while test $path != /
+#         and not test -e "$path/$argv[1]"
+#         set path (dirname $path)
+#     end
+#     if test -e "$path/$argv[1]"
+#         echo $path
+#     else
+#         echo ""
+#     end
+# end
+# function __check_nvm --on-variable PWD
+#     if status --is-command-substitution
+#         return
+#     end
+#     set nvm_path (find_up ".nvmrc" | tr -d '[:space:]')
+#     if test "$nvm_path" != ""
+#         set nvmrc_node_version (nvm version (cat "$nvm_path/.nvmrc") '; 2>1')
+#         if test "$nvmrc_node_version" = N/A
+#             echo "Installing node version "(cat "$nvm_path/.nvmrc")
+#             nvm install
+#             set nvm_node_version (nvm version)
+#         else if test "$nvmrc_node_version" != (nvm version)
+#             echo "Changing node version to $nvmrc_node_version"
+#             nvm use
+#             set nvm_node_version (nvm version)
+#         end
+#         echo "$nvm_path/.nvmrc"
+#     else if test (nvm version) = none
+#         nvm use default --silent
+#         # else if test (nvm version) != (nvm version default)
+#         # echo "Reverting node version to default"
+#         # nvm use default
+#     end
+# end
+# __check_nvm
+#
+# bass source ~/.nvm/nvm.sh >/dev/null ^ /dev/null
 
 
 
@@ -96,7 +104,7 @@ bass source ~/.nvm/nvm.sh > /dev/null ^ /dev/null;
 # pnpm
 set -gx PNPM_HOME "/home/kambi/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
 
@@ -108,3 +116,10 @@ set -x PATH $DENO_INSTALL/bin:$PATH
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# opam configuration
+source /home/kambi/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/kambi/.ghcup/bin $PATH # ghcup-env
+
+set -gx PATH $PATH /home/kambi/.local/bin
